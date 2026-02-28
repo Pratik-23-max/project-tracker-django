370e561bc1b4eb99467628c98ce3be8966f6833f
pratik valse <valsepratik913@gmail.com>
2026-02-28 15:53:55 +0530

    update MIDDLEWARE

--- my_site/settings.py
+++ my_site/settings.py
@@ -38,13 +38,11 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # Add this for static files in production!
+    # WHITE NOISE MUST GO HERE
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # ... other middleware ...
 ]
 
 ROOT_URLCONF = 'my_site.urls'
