3340ec1134f56305455673799bc9847e44056876
pratik valse <valsepratik913@gmail.com>
2026-02-28 16:29:32 +0530

    fix: restore professional dark theme and fix model typo

--- my_site/settings.py
+++ my_site/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # Must be second
+    'whitenoise.middleware.WhiteNoiseMiddleware', # For static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
