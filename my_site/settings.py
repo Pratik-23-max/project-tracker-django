4fe137c4557d9801c65ee189dcd03613de21c4de
pratik valse <valsepratik913@gmail.com>
2026-07-08 22:43:33 +0530

    Implement token authentication and secure API endpoints

--- my_site/settings.py
+++ my_site/settings.py
@@ -28,9 +28,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'tracker', # Your app
     
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- tracker/viewsets.py
+++ tracker/viewsets.py
@@ -3,41 +3,35 @@ from rest_framework.permissions import IsAuthenticated
 
 from .models import Project, Task
 from .serializers import ProjectSerializer, TaskSerializer
-from rest_framework.permissions import AllowAny
-
-
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
 
     serializer_class = ProjectSerializer
 
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
 
-        if self.request.user.is_authenticated:
-            return Project.objects.filter(
-                manager=self.request.user
-            )
-
-        return Project.objects.all()
+        return Project.objects.filter(
+            manager=self.request.user
+        )
 
     def perform_create(self, serializer):
 
-        if self.request.user.is_authenticated:
-            serializer.save(
-                manager=self.request.user
-            )
-        
+        serializer.save(
+            manager=self.request.user
+        )
+
+
 class TaskViewSet(viewsets.ModelViewSet):
 
     serializer_class = TaskSerializer
 
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
 
         return Task.objects.filter(
             assigned_to=self.request.user
-        )        
+        )
